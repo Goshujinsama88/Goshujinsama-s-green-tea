be5e47ee7e0c28cf7c80f9a7c1ae055e9e84a116
Fernando Escutia <goshujinsama88@gmail.com>
2020-07-27 00:35:03 -0500

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -22,7 +22,7 @@ done
 clear
 echo "INFO"
 echo "Goshujinsama's green tea is a theme for Cinnamon desktop manager, is based in the
-Cinnamon theme from Matcha GTK theme by vinceliuice. This theme is aimmed to Linux Mint,
+Cinnamon theme from Matcha GTK theme by vinceliuice. This theme is aimed to Linux Mint,
 so includes a modification of the GTK Theme for Nemo and other programs to match the
 Cinnamon Theme.\n"
 echo "This theme doesn't recieve any support from vinceliuice or the Matcha GTK Theme team.\n"
